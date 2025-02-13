7e28d41266f02d86f674c99c9790052fc08445ba
Celestial-Fox <stealevw@gmail.com>
2025-02-13 12:56:49 +0100

    byte

--- Assets/Scripts/Byte.cs
+++ Assets/Scripts/Byte.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Byte : MonoBehaviour
 {
     [SerializeField] Bit[] bits = new Bit[8];
     [SerializeField] private int value = 0;
+    int i = new int();
     void Start()
     {
     }
@@ -18,5 +20,13 @@ public class Byte : MonoBehaviour
     private void BinToDec()
     {
         value = 0;
+
+        for (int i = 0; i < bits.Length; i++) 
+        { 
+            if (bits[i].state)
+            {
+                value += (1 << i);
+            }
+        }
     }
 }
